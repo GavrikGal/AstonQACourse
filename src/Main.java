4cda5176b55bc0d91c79d6b2e9c2bbde37798d84
GavrikGal <gavrik.gal@gmail.com>
2024-06-30 11:15:13 +0300

    2nd Task added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("___Task 1____");
+        printThreeWords();
+
+        System.out.println("\n___Task 2____");
+        checkSumSign();
+
     }
+
+    public static void printThreeWords() {
+        List<String> words = new ArrayList<String>(
+                Arrays.asList("Orange", "Banana", "Apple")
+        );
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+    }
+
+    public static void checkSumSign() {
+        int a = 1;
+        int b = 2;
+
+        if (a + b >= 0)
+            System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+
+
 }
