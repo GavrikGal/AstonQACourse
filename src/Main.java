79edb9bf87a7eb56d60ba4f6770c258432761bc5
GavrikGal <gavrik.gal@gmail.com>
2024-07-16 01:06:42 +0300

    Lesson 9. Completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,74 @@
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        String[][] array = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int sum = calculateSum(array);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        // Я бы сделал метод для тестирования хороших и плохих исходов,
+        // но в п.3 ТЗ написано, что вызывать метод calculateSum()
+        // и обрабатывать исключения необходимо в методе main()
+        // поэтому дальше идет ctrl+C и ctrl+V,
+        // в противоположность принципу dry
+        String[][] wrongArray1 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"}
+        };
+
+        try {
+            int sum = calculateSum(wrongArray1);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+
+        String[][] wrongArray2 = {
+                {"1", "два", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            int sum = calculateSum(wrongArray2);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
+
+    public static int calculateSum(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        // Проверяем размер массива
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Размер массива должен быть 4x4");
+        }
+
+        int sum = 0;
+
+        // Проходим по элементам массива и суммируем их
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    int num = Integer.parseInt(array[i][j]);
+                    sum += num;
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" +
+                            i + "][" + j + "]: " + array[i][j]);
+                }
+            }
+        }
+
+        return sum;
     }
 }
