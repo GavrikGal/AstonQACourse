45f0fce0e28c494c05490ccd3b83fa786d1f7ee4
GavrikGal <gavrik.gal@gmail.com>
2024-07-29 22:50:22 +0300

    Lesson 10. Add Maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print(calculateFactorial(5));
-    }
-
-    public static long calculateFactorial(int n) {
-        if (n < 0) {
-            throw new IllegalArgumentException("Факториал не определен для отрицательных чисел.");
-        }
-
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        System.out.print("Hello World");
     }
 }
