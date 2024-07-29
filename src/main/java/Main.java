0ee6a06cc67c0e52b934c10ddf674e13af53c904
GavrikGal <gavrik.gal@gmail.com>
2024-07-29 23:10:50 +0300

    Lesson 10. Task 1 completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("---- Task 1 ----");
+        task1();
+
     }
+
+    public static void task1() {
+        String[] words = {
+                "яблоко", "банан", "яблоко", "апельсин", "банан",
+                "киви", "яблоко", "груша", "персик", "киви", "слива",
+                "яблоко", "слива", "банан", "виноград", "персик"
+        };
+
+        Set<String> uniqueWords = new HashSet<>();
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            uniqueWords.add(word);
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+        }
+
+        System.out.println("Уникальные слова: " + uniqueWords);
+        System.out.println("Частота встречаемости слов: " + wordCount);
+    }
+
 }
